76615a6b3fad00b320c91c858b5a8270f1253113
emmanuelkolawole4 <emmanuelkolawole4@gmail.com>
2021-02-18 10:47:29 +0100

    create UI view for login screen without validation and functionalities

---
+++ FundallCodingChallenge/FundallCodingChallenge/StringConstants/StringConstants.swift
@@ -0,0 +1,23 @@
+//
+//  StringConstants.swift
+//  FundallCodingChallenge
+//
+//  Created by FOLAHANMI KOLAWOLE on 17/02/2021.
+//
+
+import UIKit
+
+enum NavBarItemStringConstants {
+   static let benefits = "Benefits"
+   static let cancel = "Cancel"
+}
+
+enum K {
+   enum Images {
+      static let person = "person"
+      static let phone = "phone"
+      static let lock = "lock"
+      static let message = "message"
+      static let envelope = "envelope"
+   }
+}
